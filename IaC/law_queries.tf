1c384854b7b37bb3fe8eb99872ceea08ca990e75
Niclas <37627447+zamomfg@users.noreply.github.com>
2025-03-22 23:30:23 +0100

    removing diagnostic resource since it wont work with cicd pipeline, and commented out law queries for now

--- IaC/law_queries.tf
+++ IaC/law_queries.tf
@@ -12,80 +12,80 @@ resource "azurerm_log_analytics_query_pack" "query_pack" {
 }
 
 
-module "local_ip_ranges" {
-  source = "./modules/terraform-file-storage"
+# module "local_ip_ranges" {
+#   source = "./modules/terraform-file-storage"
 
-  storage_account = azurerm_storage_account.watchlist_sa
-  storage_container_name = azurerm_storage_container.watchlist_sa_container.name
-  content_file_path = "../watchlist_data/local_ip_ranges.csv"
-}
+#   storage_account = azurerm_storage_account.watchlist_sa
+#   storage_container_name = azurerm_storage_container.watchlist_sa_container.name
+#   content_file_path = "../watchlist_data/local_ip_ranges.csv"
+# }
 
-resource "azurerm_log_analytics_query_pack_query" "query_local_ip_ranges" {
+# resource "azurerm_log_analytics_query_pack_query" "query_local_ip_ranges" {
 
-  query_pack_id = azurerm_log_analytics_query_pack.query_pack.id
-  display_name = "${local.custom_query_prefix}local_IP_network"
+#   query_pack_id = azurerm_log_analytics_query_pack.query_pack.id
+#   display_name = "${local.custom_query_prefix}local_IP_network"
 
-  body = <<-EOT
-        let LocalRanges = externaldata(Name:string, IpRange:string)
-        [ 
-          h@"${module.local_ip_ranges.sas_token_url}"
-        ] with (format="csv", ignoreFirstRecord=true);
-        LocalRanges
-  EOT
-}
+#   body = <<-EOT
+#         let LocalRanges = externaldata(Name:string, IpRange:string)
+#         [ 
+#           h@"${module.local_ip_ranges.sas_token_url}"
+#         ] with (format="csv", ignoreFirstRecord=true);
+#         LocalRanges
+#   EOT
+# }
 
-resource "azurerm_log_analytics_saved_search" "func_local_ip_ranges" {
-  name                       = "${local.custom_func_prefix}local_ip_ranges"
-  function_alias             = "${local.custom_func_prefix}local_ip_ranges"
-  display_name               = "${local.custom_func_prefix}local_ip_ranges"
+# resource "azurerm_log_analytics_saved_search" "func_local_ip_ranges" {
+#   name                       = "${local.custom_func_prefix}local_ip_ranges"
+#   function_alias             = "${local.custom_func_prefix}local_ip_ranges"
+#   display_name               = "${local.custom_func_prefix}local_ip_ranges"
 
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
+#   log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
 
-  category     = "custom"
-  query        = <<-EOT
-                let LocalRanges = externaldata(Name:string, IpRange:string)
-                [ 
-                  h@"${module.local_ip_ranges.sas_token_url}"
-                ] with (format="csv", ignoreFirstRecord=true);
-                LocalRanges
-  EOT
-}
+#   category     = "custom"
+#   query        = <<-EOT
+#                 let LocalRanges = externaldata(Name:string, IpRange:string)
+#                 [ 
+#                   h@"${module.local_ip_ranges.sas_token_url}"
+#                 ] with (format="csv", ignoreFirstRecord=true);
+#                 LocalRanges
+#   EOT
+# }
 
-module "geolite_asn" {
-  source = "./modules/terraform-file-storage"
+# module "geolite_asn" {
+#   source = "./modules/terraform-file-storage"
 
-  storage_account = azurerm_storage_account.watchlist_sa
-  storage_container_name = azurerm_storage_container.watchlist_sa_container.name
-  content_file_path = "../watchlist_data/GeoLite2-ASN-Blocks-IPv4.csv"
-}
+#   storage_account = azurerm_storage_account.watchlist_sa
+#   storage_container_name = azurerm_storage_container.watchlist_sa_container.name
+#   content_file_path = "../watchlist_data/GeoLite2-ASN-Blocks-IPv4.csv"
+# }
 
-resource "azurerm_log_analytics_query_pack_query" "query_geolite_asn" {
+# resource "azurerm_log_analytics_query_pack_query" "query_geolite_asn" {
 
-  query_pack_id = azurerm_log_analytics_query_pack.query_pack.id
-  display_name = "${local.custom_query_prefix}ASN_info"
+#   query_pack_id = azurerm_log_analytics_query_pack.query_pack.id
+#   display_name = "${local.custom_query_prefix}ASN_info"
 
-  body = <<-EOT
-        let ASN = externaldata(IpRange:string, ASN:string,ASNorg:string)
-        [ 
-          h@"${module.geolite_asn.sas_token_url}"
-        ] with (format="csv", ignoreFirstRecord=true);
-        ASN
-  EOT
-}
+#   body = <<-EOT
+#         let ASN = externaldata(IpRange:string, ASN:string,ASNorg:string)
+#         [ 
+#           h@"${module.geolite_asn.sas_token_url}"
+#         ] with (format="csv", ignoreFirstRecord=true);
+#         ASN
+#   EOT
+# }
 
-resource "azurerm_log_analytics_saved_search" "func_geolite_asn" {
-  name                       = "${local.custom_func_prefix}asn_info"
-  function_alias             = "${local.custom_func_prefix}asn_info"
-  display_name               = "${local.custom_func_prefix}asn_info"
+# resource "azurerm_log_analytics_saved_search" "func_geolite_asn" {
+#   name                       = "${local.custom_func_prefix}asn_info"
+#   function_alias             = "${local.custom_func_prefix}asn_info"
+#   display_name               = "${local.custom_func_prefix}asn_info"
 
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
+#   log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
 
-  category     = "custom"
-  query        = <<-EOT
-                let ASN = externaldata(IpRange:string, ASN:string,ASNorg:string)
-                [ 
-                  h@"${module.geolite_asn.sas_token_url}"
-                ] with (format="csv", ignoreFirstRecord=true);
-                ASN
-  EOT
-}
+#   category     = "custom"
+#   query        = <<-EOT
+#                 let ASN = externaldata(IpRange:string, ASN:string,ASNorg:string)
+#                 [ 
+#                   h@"${module.geolite_asn.sas_token_url}"
+#                 ] with (format="csv", ignoreFirstRecord=true);
+#                 ASN
+#   EOT
+# }
--- IaC/log_diagnostic_settings.tf
+++ IaC/log_diagnostic_settings.tf
@@ -1,133 +1,133 @@
 
 
-# This setting cannot be set via service principal. It needs Azure CLI authentication
-resource "azurerm_monitor_aad_diagnostic_setting" "azure_ad_diagnostic_settings" {
-  name                       = "entra_id_diagnostics"
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
+## This setting cannot be set via service principal. It needs Azure CLI authentication
+# resource "azurerm_monitor_aad_diagnostic_setting" "azure_ad_diagnostic_settings" {
+#   name                       = "entra_id_diagnostics"
+#   log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
 
 
-  enabled_log {
-    category = "AuditLogs"
-    retention_policy {
-      enabled = false
-    }
-  }
+#   enabled_log {
+#     category = "AuditLogs"
+#     retention_policy {
+#       enabled = false
+#     }
+#   }
 
-  enabled_log {
-    category = "SignInLogs"
-    retention_policy {
-      enabled = false
-    }
-  }
+#   enabled_log {
+#     category = "SignInLogs"
+#     retention_policy {
+#       enabled = false
+#     }
+#   }
 
-  enabled_log {
-    category = "NonInteractiveUserSignInLogs"
-    retention_policy {
-      enabled = false
-    }
-  }
+#   enabled_log {
+#     category = "NonInteractiveUserSignInLogs"
+#     retention_policy {
+#       enabled = false
+#     }
+#   }
 
-  enabled_log {
-    category = "ServicePrincipalSignInLogs"
-    retention_policy {
-      enabled = false
-    }
-  }
+#   enabled_log {
+#     category = "ServicePrincipalSignInLogs"
+#     retention_policy {
+#       enabled = false
+#     }
+#   }
 
-  enabled_log {
-    category = "ManagedIdentitySignInLogs"
-    retention_policy {
-      enabled = false
-    }
-  }
+#   enabled_log {
+#     category = "ManagedIdentitySignInLogs"
+#     retention_policy {
+#       enabled = false
+#     }
+#   }
 
-  enabled_log {
-    category = "ProvisioningLogs"
-    retention_policy {
-      enabled = false
-    }
-  }
+#   enabled_log {
+#     category = "ProvisioningLogs"
+#     retention_policy {
+#       enabled = false
+#     }
+#   }
 
-  enabled_log {
-    category = "RiskyUsers"
-    retention_policy {
-      enabled = false
-    }
-  }
+#   enabled_log {
+#     category = "RiskyUsers"
+#     retention_policy {
+#       enabled = false
+#     }
+#   }
 
-  enabled_log {
-    category = "UserRiskEvents"
-    retention_policy {
-      enabled = false
-    }
-  }
+#   enabled_log {
+#     category = "UserRiskEvents"
+#     retention_policy {
+#       enabled = false
+#     }
+#   }
 
-  enabled_log {
-    category = "NetworkAccessTrafficLogs"
-    retention_policy {
-      enabled = false
-    }
-  }
+#   enabled_log {
+#     category = "NetworkAccessTrafficLogs"
+#     retention_policy {
+#       enabled = false
+#     }
+#   }
 
-  enabled_log {
-    category = "RiskyServicePrincipals"
-    retention_policy {
-      enabled = false
-    }
-  }
+#   enabled_log {
+#     category = "RiskyServicePrincipals"
+#     retention_policy {
+#       enabled = false
+#     }
+#   }
 
-  enabled_log {
-    category = "ServicePrincipalRiskEvents"
-    retention_policy {
-      enabled = false
-    }
-  }
+#   enabled_log {
+#     category = "ServicePrincipalRiskEvents"
+#     retention_policy {
+#       enabled = false
+#     }
+#   }
 
-  enabled_log {
-    category = "EnrichedOffice365AuditLogs"
-    retention_policy {
-      enabled = false
-    }
-  }
+#   enabled_log {
+#     category = "EnrichedOffice365AuditLogs"
+#     retention_policy {
+#       enabled = false
+#     }
+#   }
 
-  # The cost about 300 sek a week for my usage
-  enabled_log {
-    category = "MicrosoftGraphActivityLogs"
-    retention_policy {
-      enabled = false
-    }
-  }
+#   # The cost about 300 sek a week for my usage
+#   enabled_log {
+#     category = "MicrosoftGraphActivityLogs"
+#     retention_policy {
+#       enabled = false
+#     }
+#   }
 
-  enabled_log {
-    category = "RemoteNetworkHealthLogs"
-    retention_policy {
-      enabled = false
-    }
-  }
+#   enabled_log {
+#     category = "RemoteNetworkHealthLogs"
+#     retention_policy {
+#       enabled = false
+#     }
+#   }
 
-  enabled_log {
-    category = "NetworkAccessAlerts"
-    retention_policy {
-      enabled = false
-    }
-  }
-}
+#   enabled_log {
+#     category = "NetworkAccessAlerts"
+#     retention_policy {
+#       enabled = false
+#     }
+#   }
+# }
 
 
 
-# AuditLogs
-# SignInLogs
-# NonInteractiveUserSignInLogs
-# ServicePrincipalSignInLogs
-# ManagedIdentitySignInLogs
-# ProvisioningLogs
-# ADFSSignInLogs
-# RiskyUsers
-# UserRiskEvents
-# NetworkAccessTrafficLogs
-# RiskyServicePrincipals
-# ServicePrincipalRiskEvents
-# EnrichedOffice365AuditLogs
-# MicrosoftGraphActivityLogs
-# RemoteNetworkHealthLogs
-# NetworkAccessAlerts
+# # AuditLogs
+# # SignInLogs
+# # NonInteractiveUserSignInLogs
+# # ServicePrincipalSignInLogs
+# # ManagedIdentitySignInLogs
+# # ProvisioningLogs
+# # ADFSSignInLogs
+# # RiskyUsers
+# # UserRiskEvents
+# # NetworkAccessTrafficLogs
+# # RiskyServicePrincipals
+# # ServicePrincipalRiskEvents
+# # EnrichedOffice365AuditLogs
+# # MicrosoftGraphActivityLogs
+# # RemoteNetworkHealthLogs
+# # NetworkAccessAlerts
