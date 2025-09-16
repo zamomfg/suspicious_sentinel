b970814aa2fcfaea38e40315e5447f397eb888e0
Niclas <37627447+zamomfg@users.noreply.github.com>
2025-09-16 21:59:49 +0200

    stuff

--- IaC/data_source.tf
+++ IaC/data_source.tf
@@ -8,7 +8,7 @@ data "azurerm_arc_machine" "arc_log_machine" {
   resource_group_name = "rg-arc-prod-sc"
 }
 
-data "azurerm_monitor_data_collection_rule" "workspace_dcr" {
-  name = "dcr-workspace-sc-001"
-  resource_group_name = "rg-log-sc-001"
-}
+# data "azurerm_monitor_data_collection_rule" "workspace_dcr" {
+#   name = "dcr-workspace-sc-001"
+#   resource_group_name = "rg-log-sc-001"
+# }
